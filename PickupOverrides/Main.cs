5f0e2bf6b24b4e4b2e95be6971d5966394f8e030
kafeijao <kafeijao@gmail.com>
2022-09-17 22:55:00 +0100

    [PickupOverrides] Added a couple more overrides and implemented proper reverting to defaults.

--- PickupOverrides/Main.cs
+++ PickupOverrides/Main.cs
@@ -5,42 +5,113 @@ using MelonLoader;
 
 namespace PickupOverrides;
 
+internal record PickupOverrideSettings {
+    public bool AutoHold { get; set; }
+    public float MaxHoldDistance { get; set; }
+    public float MaxGrabDistance { get; set; }
+
+}
+
 public class PickupOverrides : MelonMod {
-    
+
     private static MelonPreferences_Category melonCategoryPickupOverrides;
-    private static MelonPreferences_Entry<bool> melonEntryOverride;
-    private static MelonPreferences_Entry<bool> melonEntryAutoHold;
-    
+
+    private static MelonPreferences_Entry<bool> melonEntryOverrideAutoHold;
+    private static MelonPreferences_Entry<bool> melonEntryAutoHoldValue;
+
+    private static MelonPreferences_Entry<bool> melonEntryOverrideMaxHoldDistance;
+    private static MelonPreferences_Entry<float> melonEntryMaxHoldDistanceValue;
+
+    private static MelonPreferences_Entry<bool> melonEntryOverrideMaxGrabDistance;
+    private static MelonPreferences_Entry<float> melonEntryMaxGrabDistanceValue;
+
+    private static readonly Dictionary<CVRPickupObject, PickupOverrideSettings> PickupSettings = new();
+
     public override void OnApplicationStart() {
-        
+
         // Melon Config
         melonCategoryPickupOverrides = MelonPreferences.CreateCategory(nameof(PickupOverrides));
-        melonEntryOverride = melonCategoryPickupOverrides.CreateEntry("OverrideSettings", true,
-            description: "Whether this mod should override the pickup settings or not.");
-        melonEntryAutoHold = melonCategoryPickupOverrides.CreateEntry("AutoHold", false,
+        melonEntryOverrideAutoHold = melonCategoryPickupOverrides.CreateEntry("OverrideAutoHold", true,
+            description: "Whether this mod should override the pickup auto hold setting or not.",
+            oldIdentifier: "OverrideSettings");
+        melonEntryOverrideAutoHold.OnValueChangedUntyped += UpdateAllPickups;
+        melonEntryAutoHoldValue = melonCategoryPickupOverrides.CreateEntry("AutoHold", false,
             description: "The value for the Auto Hold setting. If true the pickup will stick to your hand." +
                          "And can only be dropped by pressing G (on desktop) or holding the right controller " +
                          "grip and pull the right thumbstick down (on VR).");
+        melonEntryAutoHoldValue.OnValueChangedUntyped += UpdateAllPickups;
+
+        melonEntryOverrideMaxHoldDistance = melonCategoryPickupOverrides.CreateEntry("OverrideMaxHoldDistance",
+            false, description: "Whether this mod should override the pickup max holding distance setting " +
+                                "or not.");
+        melonEntryOverrideMaxHoldDistance.OnValueChangedUntyped += UpdateAllPickups;
+        melonEntryMaxHoldDistanceValue = melonCategoryPickupOverrides.CreateEntry("MaxHoldDistance",
+            3f, description: "The value for the max holding distance setting. This will set the max " +
+                             "distance from which the object will be from your hand when you grab. Useful to prevent " +
+                             "needing to drag your hands to bring the object closer. (default 3f)");
+        melonEntryMaxHoldDistanceValue.OnValueChangedUntyped += UpdateAllPickups;
+
+        melonEntryOverrideMaxGrabDistance = melonCategoryPickupOverrides.CreateEntry("OverrideMaxGrabDistance",
+            false, description: "Whether this mod should override the pickup maximum grabbing distance " +
+                               "setting or not.");
+        melonEntryOverrideMaxGrabDistance.OnValueChangedUntyped += UpdateAllPickups;
+        melonEntryMaxGrabDistanceValue = melonCategoryPickupOverrides.CreateEntry("MaxGrabDistance",
+            0f, description: "The value for the max grabbing distance setting. This setting will define " +
+                             "how far you can grab the pickups, this can be useful if you're tired of everything" +
+                             "being grabbable from the other side of the world. 0 for no limit (default)");
+        melonEntryMaxGrabDistanceValue.OnValueChangedUntyped += UpdateAllPickups;
+
         melonCategoryPickupOverrides.SaveToFile(false);
-        melonEntryAutoHold.OnValueChangedUntyped += UpdateAllPickups;
     }
 
     private static void UpdateAllPickups() {
-        if (!melonEntryOverride.Value) return;
-        var pickups = Traverse.Create(CVR_InteractableManager.Instance).Field("pickupList").GetValue<List<CVRPickupObject>>();
-        foreach (var pickup in pickups) {
-            pickup.autoHold = melonEntryAutoHold.Value;
+        foreach (var pickup in PickupSettings.Keys) {
+            if (pickup != null) UpdatePickup(pickup);
         }
     }
 
+    private static void UpdatePickup(CVRPickupObject pickup) {
+
+        // Update auto-hold
+        pickup.autoHold = melonEntryOverrideAutoHold.Value
+            ? melonEntryAutoHoldValue.Value
+            : PickupSettings[pickup].AutoHold;
+
+        // Update max hold distance
+        Traverse.Create(pickup).Field("maxDistance").SetValue(melonEntryOverrideMaxHoldDistance.Value
+            ? melonEntryMaxHoldDistanceValue.Value
+            : PickupSettings[pickup].MaxHoldDistance);
+
+        // Update max grab distance
+        Traverse.Create(pickup).Field("maximumGrabDistance").SetValue(melonEntryOverrideMaxGrabDistance.Value
+            ? melonEntryMaxGrabDistanceValue.Value
+            : PickupSettings[pickup].MaxGrabDistance);
+    }
+
     [HarmonyPatch]
-    private class HarmonyPatches
-    {
+    private class HarmonyPatches {
+
         [HarmonyPostfix]
         [HarmonyPatch(typeof(CVR_InteractableManager), nameof(CVR_InteractableManager.AddPickup))]
         private static void AfterAddPickup(ref CVRPickupObject pickupObject) {
-            if (!melonEntryOverride.Value) return;
-            pickupObject.autoHold = melonEntryAutoHold.Value;
+            PickupSettings.Add(pickupObject, new PickupOverrideSettings {
+                AutoHold = pickupObject.autoHold,
+                MaxHoldDistance = Traverse.Create(pickupObject).Field("maxDistance").GetValue<float>(),
+                MaxGrabDistance = Traverse.Create(pickupObject).Field("maximumGrabDistance").GetValue<float>(),
+            });
+            UpdatePickup(pickupObject);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CVR_InteractableManager), nameof(CVR_InteractableManager.RemovePickup))]
+        private static void AfterRemovePickup(ref CVRPickupObject pickupObject) {
+            PickupSettings.Remove(pickupObject);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CVR_InteractableManager), "OnSceneUnLoaded")]
+        private static void AfterUnloadingScene() {
+            PickupSettings.Clear();
         }
     }
 }
--- PickupOverrides/README.md
+++ PickupOverrides/README.md
@@ -1,17 +1,35 @@
 # PickupOverrides
-This mod allows to override certain pickup settings. Currently it only supports overriding the Auto-Hold settings.
+This mod allows to override a few settings for all the pickups in the scene.
+
+## Auto-Hold
+Whether the pickups will stick to your hand or not.
 
 With Auto-Hold set to `true` all pickups will stick to your hand once you grab them. To release you need to:
- - Press `G` on your keyboard (if playing in desktop)
- - Hold the `grip` while pulling the `thumbstick down` on your **right** controller (if playing in VR).
+- Press `G` on your keyboard (if playing in desktop)
+- Hold the `grip` while pulling the `thumbstick down` on your **right** controller (if playing in VR).
 
 If the Auto-Hold is set to `false` the pickup will be released as soon as you stop holding it.
 
-Currently the only way to change whether you want Auto-Hold or not is through Melon Config. The config is located at
-`<game_folder>\UserData\MelonPreferences.cfg`. You can edit the config while the game and it will take effect as soon
+## Maximum Held Distance
+This is how far the pickup can be from your hand. It's useful in VR to set a closer distance (like 0.5 for example) 
+because when you grab an object it will be brought close to your hand (instead of forcing us to do that weird gymnastics
+to bring it closer).
+
+## Maximum Grab Distance
+Sets the max distance you can actually grab a pickup. This is useful if you're tired of grabbing random pickups from 
+across the world. Note: The pickups will still highlight meaning they can be tele-grabbed (the alyx style pull), but you
+won't be able to grab unless it is within the max distance
+
+## Configuration
+You have always two menu settings per override, the first one says whether it should override or not  the value (if it's
+false it will revert to the pickup's default value), the second one is the value you want to set. You can edit the 
+configs in-game without any issues as it updates for all existing props in the scene.",
+
+You can use the `UI Expansion Kit` (very recommended), or by editing the the config config file located at 
+`<game_folder>\UserData\MelonPreferences.cfg`. You can change the configs while the game and it will take effect as soon 
 as you save the config file.
 
----
+
 ## Disclosure
 
 > ___
