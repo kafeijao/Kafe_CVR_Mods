f2f620806730634d1050862523772f359e2bbb0f
kafeijao <kafeijao@gmail.com>
2023-03-11 16:52:45 +0000

    Improved building instructions and script!

--- README.md
+++ README.md
@@ -168,15 +168,40 @@ Check [Instances](Instances) for more info.
 
 In order to build this project follow the instructions (thanks [@Daky](https://github.com/dakyneko)):
 
-- (1) Install NStrip.exe from https://github.com/BepInEx/NStrip into this directory (or into your PATH). This tools
-  converts all assembly symbols to public ones. Make life easy!
-- (2) Create a new Windows environment variable `CVRPATH` which should point to your game path (folder
-  where `ChilloutVR.exe` resides). In Windows, look for Settings > Advanced system settings > Advanced > Environment
-  Variables, add a new one there, it should point to something
-  like `C:\Program Files (x86)\Steam\steamapps\common\ChilloutVR` or similar.
-- (3) Run `copy_and_nstrip_dll.bat` (cmd prompt only) or `copy_and_nstrip_dll.ps1` (Powershell only) this will copy the
-game + MelonLoader .dll into this project and run NStrip.exe to make them public (easier developers).  
+- (1) If your ChilloutVR folder is `C:\Program Files (x86)\Steam\steamapps\common\ChilloutVR` you can ignore this step.
+Otherwise follow the [instructions bellow](#set-cvr-folder-environment-variable).
+- (2) Run `copy_and_nstrip_dll.ps1` on the Power Shell. This will copy the required CVR, MelonLoader, and Mod DLLs into 
+this project's `/ManagedLibs`. Note if some of the required mods are not found, it will display the url from the CVR 
+Modding Group API so you can download.
 
+### Set CVR Folder Environment Variable
+
+To build the project you need `CVRPATH` to be set to your ChilloutVR Folder, so we get the path to grab the libraries 
+we need to compile. By running the `copy_and_nstrip_dll.ps1` script that env variable is set automatically, but only
+works if the ChilloutVR folder is on the default location `C:\Program Files (x86)\Steam\steamapps\common\ChilloutVR`.
+
+Otherwise you need to set the `CVRPATH` env variable yourself, you can do that by either updating the default path in
+the `copy_and_nstrip_dll.ps1` and then run it, or manually set it via the windows menus.
+
+
+#### Setup via editing copy_and_nstrip_dll.ps1
+
+Edit `copy_and_nstrip_dll.ps1` and look the line bellow, and then replace the Path with your actual path.
+```$cvrDefaultPath = "C:\Program Files (x86)\Steam\steamapps\common\ChilloutVR"```
+
+Now you're all set and you can go to the step (2) of the [Building](#building) instructions!
+
+
+#### Setup via Windows menus
+
+In Windows Start Menu, search for `Edit environment variables for your account`, and click `New` on the top panel.
+Now you input `CVRPATH` for the **Variable name**, and the location of your ChilloutVR folder as the **Variable value**
+
+By default this value would be `C:\Program Files (x86)\Steam\steamapps\common\ChilloutVR`, but you wouldn't need to do
+this if that was the case! Make sure it points to the folder where your `ChilloutVR.exe` is located.
+
+Now you're all set and you can go to the step (2) of the [Building](#building) instructions! If you already had a power
+shell window opened, you need to close and open again, so it refreshes the Environment Variables.
 
 ---
 
--- copy_and_nstrip_dll.ps1
+++ copy_and_nstrip_dll.ps1
@@ -1,41 +1,98 @@
-$0HarmonydllPath="\MelonLoader\0Harmony.dll"
-$melonLoaderdllPath="\MelonLoader\MelonLoader.dll"
-$cvrManagedDataPath="\ChilloutVR_Data\Managed"
+# CVR and Melon Loader Dependencies
+$0HarmonydllPath    = "\MelonLoader\0Harmony.dll"
+$melonLoaderdllPath = "\MelonLoader\MelonLoader.dll"
+$cvrManagedDataPath = "\ChilloutVR_Data\Managed"
 
-# Third Party Dependencies
-$btkuiLibPath="\Mods\BTKUILib.dll"
+$cvrPath = $env:CVRPATH
+$cvrExecutable = "ChilloutVR.exee"
+$cvrDefaultPath = "C:\Program Files (x86)\Steam\steamapps\common\ChilloutVR"
 
-$cvrPath=$env:CVRPATH
+if ($cvrPath -and (Test-Path "$cvrPath\$cvrExecutable")) {
+    # Found ChilloutVR.exe in the existing CVRPATH
+    Write-Host ""
+    Write-Host "Found the ChilloutVR folder on: $cvrPath"
+}
+else {
+    # Check if ChilloutVR.exe exists in default Steam location
+    if (Test-Path "$cvrDefaultPath\$cvrExecutable") {
+        # Set CVRPATH environment variable to default Steam location
+        Write-Host "Found the ChilloutVR on the default steam location, setting the CVRPATH Env Var at User Level!"
+        [Environment]::SetEnvironmentVariable("CVRPATH", $cvrDefaultPath, "User")
+        $env:CVRPATH = $cvrDefaultPath
+        $cvrPath = $env:CVRPATH
+    }
+    else {
+        Write-Host "[ERROR] ChilloutVR.exe not found in CVRPATH or the default Steam location."
+        Write-Host "        Please define the Environment Variable CVRPATH pointing to the ChilloutVR folder!"
+        return
+    }
+}
 
 $scriptDir = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
-Write-Host "Copy all dll from the game so we can work and modify them"
+$managedLibsFolder = $scriptDir + "\ManagedLibs"
 
-
-Copy-Item $cvrPath$0HarmonydllPath -Destination $scriptDir"\ManagedLibs"
-Copy-Item $cvrPath$melonLoaderdllPath -Destination $scriptDir"\ManagedLibs"
-Copy-Item $cvrPath$cvrManagedDataPath"\*" -Destination $scriptDir"\ManagedLibs"
-
-if (Test-Path "$cvrPath$btkuiLibPath" -PathType Leaf) {
-    Copy-Item $cvrPath$btkuiLibPath -Destination $scriptDir"\ManagedLibs"
-}
-
-Write-Host "Copied all libraries!"
 Write-Host ""
-Write-Host "Press any key to strip the Dlls using NStrip"
-$HOST.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | OUT-NULL
-$HOST.UI.RawUI.Flushinputbuffer()
+Write-Host "Copying the DLLs from the CVR, MelonLoader, and Mods folder to the ManagedLibs"
 
-Write-Host "Nstrip convert all private/protected stuff to public, yay"
 
-$dllsToStrip=@('Assembly-CSharp.dll','Assembly-CSharp-firstpass.dll','UnityEngine.CoreModule.dll','Cohtml.Runtime.dll')
+Copy-Item $cvrPath$0HarmonydllPath -Destination $managedLibsFolder
+Copy-Item $cvrPath$melonLoaderdllPath -Destination $managedLibsFolder
+Copy-Item $cvrPath$cvrManagedDataPath"\*" -Destination $managedLibsFolder
 
-foreach($dllFile in $dllsToStrip)
-{
- Write-Host "stripping dll : "$dllFile
- .\NStrip.exe -p -n $cvrPath$cvrManagedDataPath"\"$dllFile $scriptDir"\ManagedLibs\"$dllFile
+
+# Third Party Dependencies
+$melonModsPath="\Mods\"
+$modNames = @("BTKUILib")
+$missingMods = New-Object System.Collections.Generic.List[string]
+
+
+foreach ($modName in $modNames) {
+    $modDll = $modName + ".dll"
+    $modPath = $cvrPath + $melonModsPath + $modDll
+    $managedLibsModPath = "$managedLibsFolder\$modDll"
+
+    # Attempt to grab from the mods folder
+    if (Test-Path $modPath -PathType Leaf) {
+        Write-Host "    Copying $modDll from $melonModsPath to \ManagedLibs!"
+        Copy-Item $modPath -Destination $managedLibsFolder
+    }
+    # Check if they already exist in the ManagedLibs
+    elseif (Test-Path $managedLibsModPath -PathType Leaf) {
+        Write-Host "    Ignoring $modDll since already exists in \ManagedLibs!"
+    }
+    # If we fail, lets add to the missing mods list
+    else {
+        $missingMods.Add($modName)
+    }
 }
 
-Write-Host "Process Completed"
+if ($missingMods.Count -gt 0) {
+    # If we have missing mods, let's fetch them from the latest CVR Modding Group API
+    Write-Host ""
+    Write-Host "Failed to find $($missingMods.Count) mods. We're going to search in CVR MG verified mods" -ForegroundColor Red
+    Write-Host "You can download them and move to $managedLibsFolder"
+
+    $cvrModdingApiUrl = "https://api.cvrmg.com/v1/mods"
+    $cvrModdingDownloadUrl = "https://api.cvrmg.com/v1/mods/download/"
+    $latestModsResponse = Invoke-RestMethod $cvrModdingApiUrl -UseBasicParsing
+
+    foreach ($modName in $missingMods) {
+        $mod = $latestModsResponse | Where-Object { $_.name -eq $modName }
+        if ($mod) {
+            $modDownloadUrl = $cvrModdingDownloadUrl + $($mod._id)
+            # It seems power shell doesn't like to download .dll from https://api.cvrmg.com (messes with some anti-virus)
+            # Invoke-WebRequest -Uri $modDownloadUrl -OutFile $managedLibsFolder -UseBasicParsing
+            # Write-Host "  $modName was downloaded successfully to $managedLibsFolder!"
+            Write-Host "    $modName Download Url: $modDownloadUrl"
+        } else {
+            Write-Host "  $modName was not found in the CVR Modding Group verified mods!"
+        }
+    }
+}
+
+
+Write-Host ""
+Write-Host "Copied all libraries!"
 Write-Host ""
 Write-Host "Press any key to exit"
 $HOST.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | OUT-NULL
