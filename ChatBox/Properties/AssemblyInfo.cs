5197e3b0331b69452fdf5e4a6677b7bd3e2d4dec
kafeijao <kafeijao@gmail.com>
2023-04-22 13:50:24 +0100

    [ChatBox] Initial Implementation.

---
+++ ChatBox/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Kafe.ChatBox;
+using Kafe.ChatBox.Properties;
+using MelonLoader;
+
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(Kafe.ChatBox))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(Kafe.ChatBox))]
+
+[assembly: MelonInfo(
+    typeof(ChatBox),
+    nameof(Kafe.ChatBox),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/kafeijao/Kafe_CVR_Mods"
+)]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: MelonColor(ConsoleColor.Green)]
+[assembly: MelonAuthorColor(ConsoleColor.DarkYellow)]
+
+namespace Kafe.ChatBox.Properties;
+internal static class AssemblyInfoParams {
+    public const string Version = "0.0.1";
+    public const string Author = "kafeijao";
+}
