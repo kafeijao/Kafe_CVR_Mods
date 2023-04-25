e6873e913615683d7f7bc1402aa8f14a531b8a5c
kafeijao <kafeijao@gmail.com>
2023-04-25 23:38:01 +0100

    [Instances] Bump version.

--- Instances/Properties/AssemblyInfo.cs
+++ Instances/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using MelonLoader;
 
 namespace Kafe.Instances.Properties;
 internal static class AssemblyInfoParams {
-    public const string Version = "1.0.3";
+    public const string Version = "1.0.4";
     public const string Author = "kafeijao";
     public const string BTKUILibName = "BTKUILib";
     public const string ChatBoxName = "ChatBox";
