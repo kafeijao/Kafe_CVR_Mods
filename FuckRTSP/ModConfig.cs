bb73bba2fade5125e420310cf325edc5b64ca811
kafeijao <kafeijao@gmail.com>
2023-05-09 22:14:09 +0100

    [FuckRTSP] Added option to disable.

---
+++ FuckRTSP/ModConfig.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+
+namespace Kafe.FuckRTSP;
+
+internal static class ModConfig {
+
+    // Melon Prefs
+    private static MelonPreferences_Category _melonCategory;
+    internal static MelonPreferences_Entry<bool> MeEnabled;
+
+    public static void InitializeMelonPrefs() {
+
+        // Melon Config
+        _melonCategory = MelonPreferences.CreateCategory(nameof(FuckRTSP));
+
+        MeEnabled = _melonCategory.CreateEntry("Enabled", true,
+            description: "Whether we should ignore RTSP videos or not.");
+
+    }
+
+}
--- FuckRTSP/Properties/AssemblyInfo.cs
+++ FuckRTSP/Properties/AssemblyInfo.cs
@@ -27,6 +27,6 @@ using MelonLoader;
 
 namespace Kafe.FuckRTSP.Properties;
 internal static class AssemblyInfoParams {
-    public const string Version = "0.0.1";
+    public const string Version = "0.0.2";
     public const string Author = "kafeijao";
 }
