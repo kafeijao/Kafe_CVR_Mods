cba557ac67b1e74617e23f4a93e0be02722955de
kafeijao <kafeijao@gmail.com>
2023-05-12 19:43:20 +0100

    [CCK.Debugger] Make the highlighter disappear after 5 seconds.

--- CCK.Debugger/Utils/Highlighter.cs
+++ CCK.Debugger/Utils/Highlighter.cs
@@ -1,48 +1,74 @@
-﻿using ABI_RC.Core.Savior;
+﻿using System.Collections;
+using ABI_RC.Core.Savior;
 using HighlightPlus;
+using MelonLoader;
 using UnityEngine;
 
 namespace Kafe.CCK.Debugger.Utils; 
 
 public static class Highlighter {
+
+    private static object _clearTimeoutCancelToken;
     
-    private static GameObject lastTarget;
-    private static HighlightProfile debugHighlight;
+    private static GameObject _lastTarget;
+    private static readonly HighlightProfile DebugHighlight;
+
+
+    private static readonly Color Blue = new Color(0f, 0.69f, 1f);
     
     static Highlighter() {
-        debugHighlight = UnityEngine.Object.Instantiate(MetaPort.Instance.worldHighlightProfile);
-        // Todo: Customize the highlight
-        //var blue = new Color(0f, 0.69f, 1f);
+        DebugHighlight = UnityEngine.Object.Instantiate(MetaPort.Instance.worldHighlightProfile);
+    }
+
+    private static IEnumerator ClearTimeout() {
+        yield return new WaitForSeconds(5f);
+        _clearTimeoutCancelToken = null;
+        ClearTargetHighlight();
     }
     
     public static void ClearTargetHighlight() {
-        if (lastTarget == null) return;
+
+        // Clear timeout if there was one going
+        if (_clearTimeoutCancelToken != null) {
+            MelonCoroutines.Stop(_clearTimeoutCancelToken);
+            _clearTimeoutCancelToken = null;
+        }
+
+        if (_lastTarget == null) return;
         
-        HighlightEffect lastHighlightEffect = lastTarget.GetComponent<HighlightEffect>();
+        HighlightEffect lastHighlightEffect = _lastTarget.GetComponent<HighlightEffect>();
         if (lastHighlightEffect == null) return;
         lastHighlightEffect.SetHighlighted(false);
-        lastTarget = null;
+        _lastTarget = null;
     }
     
     public static void SetTargetHighlight(GameObject target) {
         
         // Clear previous highlight
-        if (lastTarget != null) ClearTargetHighlight();
+        if (_lastTarget != null) ClearTargetHighlight();
         
         // Ignore if the target sent is null
         if (target == null) return;
         
         // Attempt to re-use last highlight effect (if still on), otherwise create a new one
-        HighlightEffect highlightEffect = target.GetComponent<HighlightEffect>();
-        if (highlightEffect == null)
+        var highlightEffect = target.GetComponent<HighlightEffect>();
+        if (highlightEffect == null) {
             highlightEffect = target.AddComponent<HighlightEffect>();
-        
+        }
+
         // Setup the highlight and turn it on
-        highlightEffect.ProfileLoad(debugHighlight);
+        highlightEffect.ProfileLoad(DebugHighlight);
         highlightEffect.Refresh();
         highlightEffect.SetHighlighted(true);
+
+        // Set color
+        highlightEffect.SetGlowColor(Blue);
         
         // Mark the current target as the previous target
-        lastTarget = target;
+        _lastTarget = target;
+
+        // Clear pending clear coroutine and queue another one
+        if (_clearTimeoutCancelToken != null) MelonCoroutines.Stop(_clearTimeoutCancelToken);
+        _clearTimeoutCancelToken = MelonCoroutines.Start(ClearTimeout());
     }
 }
