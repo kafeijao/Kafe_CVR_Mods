cfd9720238ae787eda21a9276f30a341b8e8c26d
kafeijao <kafeijao@gmail.com>
2023-05-15 22:04:54 +0100

    [WrongMic] Initial Implementation.

---
+++ WrongMic/Main.cs
@@ -0,0 +1,80 @@
+using ABI_RC.Core;
+using ABI_RC.Core.Savior;
+using HarmonyLib;
+using MelonLoader;
+
+namespace Kafe.WrongMic;
+
+public class WrongMic : MelonMod {
+
+    public override void OnInitializeMelon() {
+        ModConfig.InitializeMelonPrefs();
+    }
+
+    [HarmonyPatch]
+    internal class HarmonyPatches {
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(CVRTools), nameof(CVRTools.ConfigureHudAffinity))]
+        public static void After_CVRTools_ConfigureHudAffinity() {
+            // This gets called after DesktopVRSwitch mod did its thing
+            if (RootLogic.Instance != null) After_RootLogic_Start(RootLogic.Instance);
+        }
+
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(RootLogic), nameof(RootLogic.Start))]
+        public static void After_RootLogic_Start(RootLogic __instance) {
+            try {
+                if (MetaPort.Instance.isUsingVr) {
+                    if (ModConfig.MeMicVR.Value == ModConfig.Undefined) {
+                        ModConfig.MeMicVR.Value = MetaPort.Instance.settings.GetSettingsString("AudioInputDevice");
+                        MelonLogger.Warning($"There's no mic configured for VR. Going to set to the current mic [{ModConfig.MeMicVR.Value}]. " +
+                                            $"If it's wrong just change on the Game's normal menus.");
+                    }
+                    else {
+                        __instance.comms.MicrophoneName = ModConfig.MeMicVR.Value;
+                        MelonLogger.Msg($"Loaded the VR Microphone [{ModConfig.MeMicVR.Value}]");
+                    }
+                }
+                else {
+                    if (ModConfig.MeMicDesktop.Value == ModConfig.Undefined) {
+                        ModConfig.MeMicDesktop.Value = MetaPort.Instance.settings.GetSettingsString("AudioInputDevice");
+                        MelonLogger.Warning($"There's no mic configured for Desktop. Going to set to the current mic [{ModConfig.MeMicDesktop.Value}]. " +
+                                            $"If it's wrong just change on the Game's normal menus.");
+                    }
+                    else {
+                        __instance.comms.MicrophoneName = ModConfig.MeMicDesktop.Value;
+                        MelonLogger.Msg($"Loaded the Desktop Microphone [{ModConfig.MeMicVR.Value}]");
+                    }
+                }
+            }
+            catch (Exception e) {
+                MelonLogger.Error($"Error during the patched function {nameof(After_RootLogic_Start)}.");
+                MelonLogger.Error(e);
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(RootLogic), nameof(RootLogic.SettingsStringChanged))]
+        public static void After_RootLogic_SettingsStringChanged(RootLogic __instance, string name, string value) {
+            try {
+                if (name != "AudioInputDevice") return;
+                if (MetaPort.Instance.isUsingVr) {
+                    ModConfig.MeMicVR.Value = value;
+                    MelonLogger.Msg($"Set the VR Microphone to: {value}");
+                }
+                else {
+                    ModConfig.MeMicDesktop.Value = value;
+                    MelonLogger.Msg($"Set the Desktop Microphone to: {value}");
+                }
+                ModConfig.MelonCategory.SaveToFile(false);
+            }
+            catch (Exception e) {
+                MelonLogger.Error($"Error during the patched function {nameof(After_RootLogic_Start)}.");
+                MelonLogger.Error(e);
+            }
+        }
+
+    }
+}
---
+++ WrongMic/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using Kafe.WrongMic;
+using Kafe.WrongMic.Properties;
+using MelonLoader;
+
+
+[assembly: AssemblyVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyFileVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyInformationalVersion(AssemblyInfoParams.Version)]
+[assembly: AssemblyTitle(nameof(WrongMic))]
+[assembly: AssemblyCompany(AssemblyInfoParams.Author)]
+[assembly: AssemblyProduct(nameof(WrongMic))]
+
+[assembly: MelonInfo(
+    typeof(WrongMic),
+    nameof(Kafe.WrongMic),
+    AssemblyInfoParams.Version,
+    AssemblyInfoParams.Author,
+    downloadLink: "https://github.com/kafeijao/Kafe_CVR_Mods"
+)]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.MONO)]
+[assembly: MelonColor(ConsoleColor.Green)]
+[assembly: MelonAuthorColor(ConsoleColor.DarkYellow)]
+
+namespace Kafe.WrongMic.Properties;
+internal static class AssemblyInfoParams {
+    public const string Version = "0.0.1";
+    public const string Author = "kafeijao";
+}
