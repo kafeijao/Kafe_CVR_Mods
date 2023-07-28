7440221525063471ccce45a24006d4469a3feead
kafeijao <kafeijao@gmail.com>
2023-07-28 20:59:41 +0100

    [ProfilesExtended] Rebuilt with new version.

--- ProfilesExtended/Properties/AssemblyInfo.cs
+++ ProfilesExtended/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using MelonLoader;
 
 namespace Kafe.ProfilesExtended.Properties;
 internal static class AssemblyInfoParams {
-    public const string Version = "1.0.3";
+    public const string Version = "1.0.4";
     public const string Author = "kafeijao";
 }
