a04357e7de16d361bd92d615076dfd0d7880c913
kafeijao <kafeijao@gmail.com>
2023-08-03 18:24:16 +0100

    Added readableName to SteamVR actions.

--- .Common/SteamVRActions.cs
+++ .Common/SteamVRActions.cs
@@ -6,28 +6,42 @@ namespace Kafe.Common;
 
 public static class SteamVRActions {
 
-    public static void LoadAction(string actionName) {
+    public static void LoadAction(string actionName, string readableName) {
 
         var actionPath = $"/actions/AlphaBlendInteractive/in/{actionName}";
 
-        if (!SteamVR_Input.initialized || SteamVR_Input.HasAction(actionPath)) return;
+        if (!SteamVR_Input.initialized || SteamVR_Input.HasAction(actionPath)) {
+            return;
+        }
 
         var actionsFilePath = SteamVR_Input.GetActionsFilePath();
         var json = File.ReadAllText(actionsFilePath);
         var actionsFile = JsonConvert.DeserializeObject<ActionsFile>(json);
 
         if (actionsFile.actions.Exists(a => a.name == actionPath)) {
+            MelonLogger.Msg($"{actionPath} is already present in SteamVR bindings.");
             return;
         }
 
-        MelonLogger.Msg($"Didn't find {actionPath} in SteamVR bindings, adding it...");
+        MelonLogger.Msg($"Didn't find {actionPath} in SteamVR bindings, adding it with the name {readableName}...");
 
+        // Add action
         var newAction = new Action {
             name = actionPath,
             type = "boolean",
             requirement = "optional"
         };
         actionsFile.actions.Add(newAction);
+
+        // Add action localization - find English localization, if it doesn't exist create one
+        var englishLocalization = actionsFile.localization.Find(l => l.ContainsKey("language_tag") && l["language_tag"] == "en_US");
+        if (englishLocalization == null) {
+            englishLocalization = new Dictionary<string, string> {{"language_tag", "en_US"}};
+            actionsFile.localization.Add(englishLocalization);
+        }
+        englishLocalization[actionPath] = readableName;
+
+
         json = JsonConvert.SerializeObject(actionsFile, Formatting.Indented);
         File.WriteAllText(actionsFilePath, json);
         SteamVR_Input.InitializeFile(true, true);
