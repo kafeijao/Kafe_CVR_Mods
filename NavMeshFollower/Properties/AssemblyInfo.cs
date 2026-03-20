ec6b7a5526b5b8061537f867cfef4b9f4451873e
kafeijao <kafeijao@gmail.com>
2026-03-20 13:02:14 +0000

    [NavMeshFollower] Bump version

--- NavMeshFollower/Properties/AssemblyInfo.cs
+++ NavMeshFollower/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using MelonLoader;
 namespace Kafe.NavMeshFollower.Properties;
 internal static class AssemblyInfoParams
 {
-    public const string Version = "1.0.1";
+    public const string Version = "1.0.2";
     public const string Author = "kafeijao";
     public const string NavMeshToolsName = "NavMeshTools";
     public const string RequestLibName = "RequestLib";
